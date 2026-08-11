7b5e74c4eb7303a9df6607681ef36840f6cea08a
commandblock2 <tony_hu@live.com>
2026-08-11 09:13:51 +0800

    chore: jump factor

--- codegen/lib/code/blocks.py
+++ codegen/lib/code/blocks.py
@@ -144,6 +144,10 @@ def generate_blocks(
         if friction != 0.6:
             behavior_constructor += f".friction({friction})"
 
+        jump_factor = block_data_pumpkin.get("jump_velocity_multiplier")
+        if jump_factor and jump_factor != 1:
+            behavior_constructor += f".jump_factor({jump_factor})"
+
         speed_factor = block_data_pumpkin.get("velocity_multiplier")
         if speed_factor and speed_factor != 1:
             behavior_constructor += f".speed_factor({speed_factor})"
